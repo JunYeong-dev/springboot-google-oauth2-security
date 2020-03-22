923907259dcc290367a508ea6bfc6500c409e5c7
Jun-cloud <kjy8827@gmail.com>
2020-03-22 17:55:59 +0900

    Create main application

--- src/main/java/google/oauth2/security/Oauth2Application.java
+++ src/main/java/google/oauth2/security/Oauth2Application.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurityApplication {
+public class Oauth2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SecurityApplication.class, args);
+        SpringApplication.run(Oauth2Application.class, args);
     }
-
 }
